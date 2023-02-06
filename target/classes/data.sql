642815a327925edbc381cd1f372046ebe446005c
GSILVA <gsilva_dev@outlook.es>
2023-02-06 10:07:14 -0500

    ajustes

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into asignacion (nombre) values ('asignacion1');
+insert into asignacion (nombre) values ('asignacion2');
+insert into asignacion (nombre) values ('asignacion3');
+insert into asignacion (nombre) values ('asignacion4');
+insert into asignacion (nombre) values ('asignacion5');
