99af47db130be3c1f3dd261f985634f18c238161
Angel Rivera <riverareynaangel@gmail.com>
2023-02-24 23:23:11 -0500

    OracleConexion

---
+++ OracleConexion/OracleConexion/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleConexion
+{
+    partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
