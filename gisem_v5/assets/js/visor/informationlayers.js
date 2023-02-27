03737bbb08cd5e613460c230a855e3889b85682c
GSILVA <gsilva_dev@outlook.es>
2023-02-27 02:25:59 -0500

    ajustes

--- gisem_v5/assets/js/visor/informationlayers.js
+++ gisem_v5/assets/js/visor/informationlayers.js
@@ -10,6 +10,9 @@ define([
     "esri/request",
     "esri/Graphic",
     "esri/PopupTemplate",
+    "esri/Basemap",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Search",
     "dojo/dom-construct"
 ], (
     Services,
@@ -23,21 +26,43 @@ define([
     esriRequest,
     Graphic,
     PopupTemplate,
+    Basemap,
+    BasemapGallery,
+    Search,
     domConstruct
 ) => {
-    var __equipo_secc = Services.getLayerSeccionamiento();
+    var _equipo_secc_equipo = Services.getLayerSeccionamientoEquipo();
+    var _equipo_secc_tramo = Services.getLayerSeccionamientoTramo();
     var Map = visor.getMap();
     var View = visor.getView();
-    Map.add(__equipo_secc);
+    Map.addMany([_equipo_secc_equipo, _equipo_secc_tramo]);
     layerList = new LayerList({
         container: document.createElement("div"),
         view: View
+    });
+    const customBasemap = new Basemap({
+        portalItem: {
+            id: "46a87c20f09e4fc48fa3c38081e0cae6"
+        }
+    })
+    const basemapGallery = new BasemapGallery({
+        view: View,
+        source: [Basemap.fromId("topo-vector"), Basemap.fromId("hybrid"), customBasemap] // autocasts to LocalBasemapsSource
     });
     layerListExpand = new Expand({
         expandIconClass: "esri-icon-layers", // see https://developers.arcgis.com/javascript/latest/guide/esri-icon-font/
         // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
         view: View,
         content: layerList
+    });
+    const searchWidget = new Search({
+        view: View
+    });
+    baseMapExpand = new Expand({
+        expandIconClass: "esri-icon-basemap", // see https://developers.arcgis.com/javascript/latest/guide/esri-icon-font/
+        // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+        view: View,
+        content: basemapGallery
     });
     let legend = new Legend({
         view: View
@@ -48,8 +73,12 @@ define([
         view: View,
         content: legend
     });
+    View.ui.add(searchWidget, {
+        position: "top-right"
+    });
     View.ui.add(legendExpand, "top-right");
     View.ui.add(layerListExpand, "top-right");
+    View.ui.add(baseMapExpand, "top-right");
 
     layerList.when(async() => {
         await Queryjs.getQueryLayerGetEmpresa();
