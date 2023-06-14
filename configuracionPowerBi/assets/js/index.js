d84dd846d22a65150f8e605e51b3f6c8817217a7
pierriv <42954928+pierriv@users.noreply.github.com>
2023-06-14 08:53:58 -0500

    new project

---
+++ configuracionPowerBi/assets/js/index.js
@@ -0,0 +1,10 @@
+let map, view;
+let isOffice = true;
+let codeUbigeo = "";
+let urlServicePuertos = "https://gisem.osinergmin.gob.pe/validar/puerto/apipuertos/puerto";
+let chartsMonth = null;
+
+$(document).ready(function() {
+    console.log('data');
+    $('.loading').hide();
+});
