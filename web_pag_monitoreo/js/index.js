1ae85e45ce59a75da6e61901964079e8a6b0d50b
Pier Rivera <riverareynaangel@gmail.com>
2023-10-07 13:04:11 -0500

    generacion de widgets

--- web_pag_monitoreo/js/index.js
+++ web_pag_monitoreo/js/index.js
@@ -2,6 +2,7 @@ require(
   [
     "esri/Map",
     "esri/views/MapView",
+    "esri/views/SceneView",
     "esri/identity/IdentityManager",
     "esri/core/urlUtils",
     "esri/layers/FeatureLayer",
@@ -9,18 +10,28 @@ require(
     "esri/tasks/support/Query",
     "esri/core/watchUtils",
     "dojo/_base/array",
-    "dojo/domReady!"
+    "dojo/domReady!",
+    "esri/widgets/Expand",
+    "esri/widgets/Home",
+    "esri/widgets/Zoom",
+    "esri/widgets/BasemapGallery"
   ],
   function(
     Map,
     MapView,
+    SceneView,
     IdentityManager,
     urlUtils, 
     FeatureLayer,
     QueryTask,
     Query,
     watchUtils,
-    array
+    array,
+    domReady,
+    Expand,
+    Home,
+    Zoom,
+    BasemapGallery
   ){
 
     $(document).ready(function(){     
@@ -35,7 +46,6 @@ require(
         ground: "world-elevation"
       });
   
-      
       var view = new MapView({
         container: "map",
         map: map,
@@ -49,6 +59,36 @@ require(
           }
         }
       });
+
+      //const view = new SceneView({
+      //  container: "map",
+      //  map: map3d,
+      //  zoom: 6,
+      //  center: [-72,-10]
+      //});
+
+      var homeBtn = new Home({
+        view: view
+      })
+      basemapGallery = new Expand({
+        expandIcon: "home",  // see https://developers.arcgis.com/calcite-design-system/icons/
+        // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+        view: view,
+        content: new BasemapGallery({
+          view: view
+        })
+      });
+            // Add the widget to the top-right corner of the view
+      
+
+      // Add the home button to the top left corner of the view
+      view.ui.add(homeBtn, "top-right");
+      //view.ui.add(zoom, "top-right");
+      view.ui.move([ "zoom" ], "top-right");
+
+      view.ui.add(basemapGallery, "top-right");
+
+      
 
     });
     
